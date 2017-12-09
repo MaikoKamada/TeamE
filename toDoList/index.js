90723536da564b4f1dd85059fd7152a1e371edaf
ktkth5 <zcdjkjfygxnc9w88mmt7@gmail.com>
2017-12-09 13:31:00 +0900

    change

--- toDoList/index.js
+++ toDoList/index.js
@@ -9,14 +9,6 @@ for (i=0; i<myNodelist.length; i++){
   myNodelist[i].appendChild(span);
 }
 
-var close = document.getElementsByClassName("close");
-var i;
-for (i=0; i<close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
 
 var list = document.querySelector("ul");
 list.addEventListener("click", function(ev) {
@@ -38,16 +30,37 @@ function newElement(){
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+  //[li⇨span_1⇨image_1],[li⇨span_2⇨image_2]っていうツリー構造
+  var span_1 = document.createElement("SPAN");
+  var span_2 = document.createElement("SPAN");
+  var image_1 = document.createElement("img");
+  var image_2 = document.createElement("img");
+  image_1.src="ecalic009_029.png";
+  image_2.src = "ecalic009_010.png";
+  span_1.className="close";
+  span_2.className="check";
+  span_1.appendChild(image_1);
+  span_2.appendChild(image_2);
+  li.appendChild(span_1);
+  li.appendChild(span_2);
 
+  var close = document.getElementsByClassName("close");
+  var i;
   for (i=0; i<close.length; i++){
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
+
+  var check = document.getElementsByClassName("check");
+  var j;
+  for (j=0; j<check.length; j++){
+    check[j].onclick = function() {
+      var image_3 = document.createElement("img");
+      image_2.src = "ecalic009_007.png";
+
+    }
+  }
+
 }
